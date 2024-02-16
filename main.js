617e12baa566f68b22d5c2b7c86ff7d4b06f7dff
Marcus Jaschen <mjaschen@gmail.com>
2024-02-16 11:30:01 +0100

    code style

--- main.js
+++ main.js
@@ -1,20 +1,14 @@
 if ("serviceWorker" in navigator) {
     window.addEventListener("load", async () => {
-
         try {
             let reg;
 
             if (document.location.hostname === "127.0.0.1") {
-                reg = await navigator.serviceWorker.register(
-                    "/service-worker.js",
-                    {
-                        type: "module",
-                    }
-                );
+                reg = await navigator.serviceWorker.register("/service-worker.js", {
+                    type: "module",
+                });
             } else {
-                reg = await navigator.serviceWorker.register(
-                    "/service-worker.js"
-                );
+                reg = await navigator.serviceWorker.register("/service-worker.js");
             }
         } catch (err) {
             console.log("😥 Service worker registration failed: ", err);
